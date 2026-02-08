4e4ea7e275bd3fda65063d43c6fd592a74560afe
Duc-Khanh To, Ph.D <toduc@stat.unipd.it>
2026-02-08 09:29:19 +0700

    news 8/2

--- R/bootstrap.R
+++ R/bootstrap.R
@@ -1,23 +1,50 @@
+# function for making bootstrap sample
 my_boot <- function(data, R = 500, seed = NULL){
   if(is.null(seed)) seed <- 34
   set.seed(seed)
+  data_bts <- list() # list-object
   if(is.null(dim(data))){
     n <- length(data)
     id_bts <- replicate(n = R, 
                         expr = sample(1:n, size = n, replace = TRUE))
-    data_bts <- matrix(0, nrow = n, ncol = R)
     for(i in 1:R){
-      data_bts[, i] <- data[id_bts[, i]]
+      data_bts[[i]] <- data[id_bts[, i]]
     }
   } else{
     n <- nrow(data) # dim(data)[1] # number's rows 
     id_bts <- replicate(n = R, 
                         expr = sample(1:n, size = n, replace = TRUE))
-    data_bts <- list() # list-object
     for(i in 1:R){
       data_bts[[i]] <- data[id_bts[, i], ]
     }
   }
   return(data_bts)
+}
+
+my_ci_boot <- function(boot_sample, data, fun, alpha = 0.05, 
+                       ci_type = c("quantile", "bca"), ...){
+  ci_type <- match.arg(ci_type) ## defaut is quantile
+  est_boot <- sapply(boot_sample, function(x) fun(x, ...))
+  est_boot_st <- sort(est_boot)
+  R <- length(boot_sample)
+  alpha2 <- alpha/2
+  if (ci_type == "quantile"){
+    ci_boot <- est_boot_st[c(floor(R * alpha2), floor(R * (1 - alpha2)))]
+  }
+  if (ci_type == "bca"){
+    est_sample <- fun(data, ...)
+    b_est <- qnorm(mean(est_boot <= est_sample))
+    est_jack <- sapply(1:length(data), function(i){
+      fun(data[-i], ...)
+    })
+    est_jack2 <- mean(est_jack)
+    u <- est_jack2 - est_jack
+    a_est <- ((1/6) * sum(u^3)) * (sum(u^2))^(-3/2)
+    z_alp <- qnorm(alpha2)
+    beta1_est <- pnorm(b_est + 1/(1/(b_est + z_alp) - a_est))
+    beta2_est <- pnorm(b_est + 1/(1/(b_est - z_alp) - a_est))
+    ci_boot <- est_boot_st[c(floor(R * beta1_est), floor(R * beta2_est))]
+  }
+  return(list(est_boot = est_boot, ci_type = ci_type, ci_boot = ci_boot))
 }
 
--- R/test_bootstrap.R
+++ R/test_bootstrap.R
@@ -6,12 +6,13 @@ my_boot(data = x, R = 5)
 y <- data.frame(x1 = c(1:10), x2 = rnorm(10))
 my_boot(data = y, R = 5)
 
-## Gamma distribution
+## Gamma distribution ----
 
 x <- rgamma(n = 20, shape = 3, rate = 0.5)
 x_bts <- my_boot(data = x, R = 500)
 x_bts
-x_mean_bts <- apply(x_bts, MARGIN = 2, FUN = mean)
+x_mean_bts <- sapply(x_bts, FUN = mean)
+
 hist(x_mean_bts)
 mean(x)
 mean(x_mean_bts)
@@ -20,4 +21,43 @@ x_mean_bts_s <- sort(x_mean_bts)
 x_mean_bts_s[c(floor(500*0.05/2), floor(500*(1 - 0.05/2)))]
 
 quantile(x = x_mean_bts, probs = c(0.05/2, 1 - 0.05/2), type = 3)
+
+
+## BCa ----
+x_mean <- mean(x)
+b_est <- qnorm(mean(x_mean_bts <= x_mean))
+b_est
+
+# x_mean_jack <- numeric(length(x))
+# for(i in 1:length(x)) {
+#   x_mean_jack[i] <- mean(x[-i])
+# }
+
+x_mean_jack <- sapply(1:length(x), function(i){
+  mean(x[-i])
+})
+
+x_mean_jack2 <- mean(x_mean_jack)
+u <- x_mean_jack2 - x_mean_jack
+
+a_est <- ((1/6) * sum(u^3)) * (sum(u^2))^(-3/2)
+a_est
+
+alpha <- 0.05
+z_alp <- qnorm(alpha/2)
+
+beta1_est <- pnorm(b_est + 1/(1/(b_est + z_alp) - a_est))
+beta2_est <- pnorm(b_est + 1/(1/(b_est - z_alp) - a_est))
+
+x_mean_bts_s <- sort(x_mean_bts)
+x_mean_bts_s[c(floor(500*beta1_est), floor(500*beta2_est))]
+
+test1 <- function(fun, x){
+  sapply(1:length(x), FUN = function(i) fun(x[-i]))
+}
+test1(fun = mean, x = x)
+test1(fun = median, x = x)
+
+my_ci_boot(x_bts, fun = mean)
+
 
